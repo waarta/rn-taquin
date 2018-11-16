fcfdeb297086c491f31b9b136a46440da4b7488e
PERIN <perin011@pc02C.iut-rcc-info.urca>
2018-11-16 17:01:33 +0100

    image picker a revoir

--- src/PictureSelector.js
+++ src/PictureSelector.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Button } from "react-native";
 import PropTypes from "prop-types";
+import ImagePicker from "react-native-image-picker";
 
 class PictureSelector extends Component {
 	componentDidUpdate(prevProps) {
 		if (this.props.changePic !== prevProps.changePic) {
+			ImagePicker.showImagePicker(options, response => {
+				console.log("Response = ", response);
+
+				if (response.didCancel) {
+					console.log("User cancelled image picker");
+				} else if (response.error) {
+					console.log("ImagePicker Error: ", response.error);
+				} else if (response.customButton) {
+					console.log("User tapped custom button: ", response.customButton);
+				} else {
+					const source = { uri: response.uri };
+					// You can also display the image using data:
+					// const source = { uri: 'data:image/jpeg;base64,' + response.data };
+					this.setState({
+						avatarSource: source
+					});
+				}
+			});
 			this.changeImage();
 		}
 	}
@@ -31,6 +50,14 @@ PictureSelector.propTypes = {
 	changerPicture: PropTypes.func,
 	changePic: PropTypes.bool,
 	setPicture: PropTypes.func
+};
+const options = {
+	title: "Select Picture",
+	customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
+	storageOptions: {
+		skipBackup: true,
+		path: "images"
+	}
 };
 
 const imgs = [
